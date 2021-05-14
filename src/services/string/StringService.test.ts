d311c047dd963fcca0ec5824cd74806b78f4b5a7
CraftsDev <admin@craftsdev.com>
2021-05-13 22:00:44 -0500

    clean up css safe name test

--- src/services/string/StringService.test.ts
+++ src/services/string/StringService.test.ts
@@ -1,17 +1,18 @@
 import StringService from './StringService';
 
 test('converts string to CSS safe name', () => {
-  const str = StringService.toCSSName('Invalid CSS!!@#$%^&*()_+0001');
-  expect(!str.includes(' '));
-  expect(!str.includes('!'));
-  expect(!str.includes('@'));
-  expect(!str.includes('$'));
-  expect(!str.includes('%'));
-  expect(!str.includes('^'));
-  expect(!str.includes('&'));
-  expect(!str.includes('*'));
-  expect(!str.includes('('));
-  expect(!str.includes(')'));
-  expect(!str.includes('+'));
+  const invalidCSSName = 'Invalid CSS!!`{}[]\\|:";\'<>?,./@#$%^&*()_+0001';
+  const str = StringService.toCSSName(invalidCSSName);
+  expect(str.includes(' ')).toBeFalsy();
+  expect(str.includes('!')).toBeFalsy();
+  expect(str.includes('@')).toBeFalsy();
+  expect(str.includes('$')).toBeFalsy();
+  expect(str.includes('%')).toBeFalsy();
+  expect(str.includes('^')).toBeFalsy();
+  expect(str.includes('&')).toBeFalsy();
+  expect(str.includes('*')).toBeFalsy();
+  expect(str.includes('(')).toBeFalsy();
+  expect(str.includes(')')).toBeFalsy();
+  expect(str.includes('+')).toBeFalsy();
   expect(str).toEqual('invalid-css-0001');
 });
